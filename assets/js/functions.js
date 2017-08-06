1071de04c2bfcabddc52073a2255292c31bc2498
yuliwyl <yuliwyl@gmail.com>
2017-08-06 13:40:58 +0800

    1340 digital section hover works

--- assets/js/functions.js
+++ assets/js/functions.js
@@ -1,3 +1,21 @@
+$(function() {
+
+  designBGStuff();
+
+});
+
+function designBGStuff() {
+
+  $('.app-img-link').hover(function() {
+    $(this).parent().parent().parent().css('background-color',$(this).data('color'));
+  }, function(){
+    $(this).parent().parent().parent().css('background-color',$(this).parent().parent().parent().data('orig-color'));
+  });
+}
+
+
+//wscroll
+
 $(window).scroll(function(){
     videoscroll();
 });
