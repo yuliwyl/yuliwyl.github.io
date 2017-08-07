abaf326683986a03ff43d87ccca5659a506f4a10
yuliwyl <yuliwyl@gmail.com>
2017-08-07 11:15:58 +0800

    mobile nav toggle added, header prob still needs to be fixed

--- assets/js/functions.js
+++ assets/js/functions.js
@@ -3,9 +3,18 @@ $(function() {
   designBGStuff();
   setInterval(function(){articleTada()}, 3000);
   articleTada();
-
+  mobileNav();
 
 });
+
+function mobileNav(){
+  $('.mobile-nav-toggle').on('click',function(){
+    var status = $(this).hasClass('is-open');
+    if(status){ $('.mobile-nav-toggle,.mobile-nav').removeClass('is-open');}
+    else {$('.mobile-nav-toggle,.mobile-nav').addClass('is-open');}
+  });
+}
+
 
 function articleTada(){
   var randNum = Math.floor(Math.random() * $('.photo-thumb').length)+1
