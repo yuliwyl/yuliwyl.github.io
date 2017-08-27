413aae213c82cd3340954333dd6ce3efce81c3fb
yuliwyl <yuliwyl@gmail.com>
2017-08-27 11:55:30 +0800

    update from portfolio

---
+++ _site/assets/js/functions.js
@@ -0,0 +1,82 @@
+$(function() {
+
+  designBGStuff();
+  setInterval(function(){articleTada()}, 3000);
+  smoothScroll(300);
+  articleTada();
+  mobileNav();
+
+});
+
+function smoothScroll (duration) {
+	$('a[href^="#"]').on('click', function(event) {
+
+	    var target = $( $(this).attr('href') );
+
+	    if( target.length ) {
+	        event.preventDefault();
+	        $('html, body').animate({
+	            scrollTop: target.offset().top
+	        }, duration);
+	    }
+	});
+}
+
+
+function mobileNav(){
+  $('.mobile-nav-toggle').on('click',function(){
+    var status = $(this).hasClass('is-open');
+    if(status){ $('.mobile-nav-toggle,.mobile-nav').removeClass('is-open');}
+    else {$('.mobile-nav-toggle,.mobile-nav').addClass('is-open');}
+  });
+}
+
+
+function articleTada(){
+  var randNum = Math.floor(Math.random() * $('.photo-thumb').length)+1
+
+  $('.photo-thumb').eq(randNum).addClass('is-emph')
+    .siblings().removeClass('is-emph');
+
+}
+
+function designBGStuff() {
+
+  $('.app-img-link').hover(function() {
+    $(this).parent().parent().parent().css('background-color',$(this).data('color'));
+  }, function(){
+    $(this).parent().parent().parent().css('background-color',$(this).parent().parent().parent().data('orig-color'));
+  });
+}
+
+
+//wscroll
+
+$(window).scroll(function(){
+    videoscroll();
+    startPhoto();
+});
+
+function videoscroll() {
+    var wscroll = $(window).scrollTop();
+
+    $('.video-strip').css('background-position', 'center -' + wscroll + 'px')
+
+}
+
+function startPhoto(){
+
+  var wscroll = $(window).scrollTop();
+
+  if($('section.photo').offset().top - $(window).height()/2 < wscroll) {
+
+    $('.photo-thumb').each(function(i){
+
+      setTimeout(function(){
+
+        $('.photo-thumb').eq(i).addClass('is-visible');
+
+      }, 160 * i);
+    });
+  }
+}
